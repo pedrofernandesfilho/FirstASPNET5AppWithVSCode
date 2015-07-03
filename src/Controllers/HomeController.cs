cc17ab4bf770fc304106c855923a3fcae9f25747
pedrofernandesfilho <pedro.fernandesfilho@gmail.com>
2015-07-03 20:30:40 -0300

    Adding project

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using FirstASPNET5WebAppWithVSCode.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace FirstASPNET5WebAppWithVSCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MenuItem[] menuItems = {
+                 new MenuItem { Label = "Page A", Url = "pageA.html" }
+                ,new MenuItem { Label = "Page B", Url = "pageB.html" }
+            };
+            
+            return View(menuItems);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ src/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+namespace FirstASPNET5WebAppWithVSCode.Models
+{
+	public class MenuItem
+	{
+		public int MenuItemId { get; set; }
+		public string Label { get; set; }
+		public string Url { get; set; }
+	}
+}
